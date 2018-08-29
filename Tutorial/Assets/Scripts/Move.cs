fa9d6a38da63a8d46a93802299324d02c3476131
armandoGutierrez <gutierrezjarr@gmail.com>
2018-08-29 15:33:10 -0800

    Rotando cubo 2
    Se rota el cubo, de acuerdo a la tecla presionada

--- Tutorial/Assets/Scripts/Move.cs
+++ Tutorial/Assets/Scripts/Move.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour {
+public class Move : MonoBehaviour
+{
 
+	public float deltaRotate = 30;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.Rotate(new Vector3(0f, 30f, 0f) * Time.deltaTime);
+	void Update ()
+	{
+		Rotate();
 	}
+
+	void Rotate()
+	{
+		if (Input.GetKey(KeyCode.Q))
+		{
+			transform.Rotate(new Vector3(0f, deltaRotate, 0f) * Time.deltaTime);
+		}
+		else
+		{
+			if (Input.GetKey(KeyCode.E))
+			{
+				transform.Rotate(new Vector3(0f, -deltaRotate, 0f) * Time.deltaTime);
+			}
+		}
+	}
+	
 }
